958e03bbe8403e6ff6a000dfc87171d68190b662
Filip Taczkowski <filip.taczkowski@gmail.com>
2020-02-13 19:02:01 +0100

    Update RWD for MenuBar and add hamburger menu

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -2,50 +2,78 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ProductSearch from '../../features/ProductSearch/ProductSearch';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './MenuBar.module.scss';
 
-const MenuBar = ({ children }) => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row align-items-center'>
-        <div className='col'>
-          <ProductSearch />
-        </div>
-        <div className={'col-auto ' + styles.menu}>
-          <ul>
-            <li>
-              <a href='#' className={styles.active}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#'>Furniture</a>
-            </li>
-            <li>
-              <a href='#'>Chair</a>
-            </li>
-            <li>
-              <a href='#'>Table</a>
-            </li>
-            <li>
-              <a href='#'>Sofa</a>
-            </li>
-            <li>
-              <a href='#'>Bedroom</a>
-            </li>
-            <li>
-              <a href='#'>Blog</a>
-            </li>
-          </ul>
+class MenuBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuToggled: false,
+    };
+  }
+
+  handleToggle() {
+    this.setState({ menuToggled: !this.state.menuToggled });
+  }
+
+  toggledMenu() {
+    if (this.state.menuToggled) {
+      return styles.toggled;
+    } else {
+      return '';
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <div className={'container ' + styles.box}>
+          <div className={'row align-items-center ' + styles.box}>
+            <div className={'col ' + styles.col}>
+              <ProductSearch />
+            </div>
+            <button className={styles.hamburger} onClick={() => this.handleToggle()}>
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+            <div
+              className={
+                'col-auto ' + styles.menu + ' ' + styles.col + ' ' + this.toggledMenu()
+              }
+            >
+              <ul>
+                <li>
+                  <a href='#' className={styles.active}>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href='#'>Furniture</a>
+                </li>
+                <li>
+                  <a href='#'>Chair</a>
+                </li>
+                <li>
+                  <a href='#'>Table</a>
+                </li>
+                <li>
+                  <a href='#'>Sofa</a>
+                </li>
+                <li>
+                  <a href='#'>Bedroom</a>
+                </li>
+                <li>
+                  <a href='#'>Blog</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
-
-MenuBar.propTypes = {
-  children: PropTypes.node,
-};
+    );
+  }
+}
 
 export default MenuBar;

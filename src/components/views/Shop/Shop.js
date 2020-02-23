c5145cab016a2e568f0adbd43c5ca821f46e5a10
Pawel Bozemski <paw.boz@gmail.com>
2020-02-23 18:31:18 +0100

    Add Newsletter

--- src/components/views/Shop/Shop.js
+++ src/components/views/Shop/Shop.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ShopBox from '../../common/ShopBox/ShopBox';
 import ShopTable from '../../common/ShopTable/ShopTable';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import Newsletter from '../../common/Newsletter/Newsletter';
 
-class Shop extends React.Component {
-  render() {
-    return (
-      <div>
-        <ShopBox />
-        <ShopTable />
-        <NewFurniture />
-      </div>
-    );
-  }
-}
+const Shop = () => (
+  <div>
+    <ShopBox />
+    <ShopTable />
+    <NewFurniture />
+    <Newsletter />
+  </div>
+);
 
 export default Shop;

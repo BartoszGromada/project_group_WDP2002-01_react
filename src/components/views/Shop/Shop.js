08383bfd6c7a52ca4c479b6bef776414c56b945e
Pawel Bozemski <paw.boz@gmail.com>
2020-02-23 14:37:16 +0100

    Add Shop component, and routing

---
+++ src/components/views/Shop/Shop.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import styles from './Shop.module.scss';
+
+const Shop = () => <div className={styles.root}>This is Shop</div>;
+
+// ProductPage.propTypes = {};
+
+export default Shop;

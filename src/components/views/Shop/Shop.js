0b73c0391bfd1f77303cc25d882ea64c5da08fc9
Pawel Bozemski <paw.boz@gmail.com>
2020-02-23 14:49:55 +0100

    Install react bootstrap

--- src/components/views/Shop/Shop.js
+++ src/components/views/Shop/Shop.js
@@ -1,8 +1,25 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './Shop.module.scss';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const Shop = () => <div className={styles.root}>This is Shop</div>;
+const Shop = () => (
+  <Jumbotron className={styles.root}>
+    <Container>
+      <Row>
+        <Col lg={2} sm={12}>
+          <h2 className={styles.sectionTitle}>Furniture</h2>
+        </Col>
+        <Col md={{ span: 3, offset: 6 }}>
+          <p>{'Home > Furniture > Chair'}</p>
+        </Col>
+      </Row>
+    </Container>
+  </Jumbotron>
+);
 
 // ProductPage.propTypes = {};
 

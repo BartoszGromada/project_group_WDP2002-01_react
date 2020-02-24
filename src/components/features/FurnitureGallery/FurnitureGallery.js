0849e0182a794b4a80b2465afefbb74e005e3328
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-02-24 22:15:11 +0100

    Change function to cleass component

--- src/components/features/FurnitureGallery/FurnitureGallery.js
+++ src/components/features/FurnitureGallery/FurnitureGallery.js
@@ -6,52 +6,58 @@ import PromoProduct from './../PromoProduct/PromoProductContainer';
 import FurnitureGalleryActions from '../FurnitureGalleryActions/FurnitureGalleryActions';
 import FurnitureGalleryPrice from '../FurnitureGalleryPrice/FurnitureGalleryPrice';
 
-const FurnitureGallery = ({ tabs, products }) => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row'>
-        <div className={`col-12 col-md-12 col-lg-6 ${styles.gallery}`}>
-          <h3>Furniture Gallery</h3>
-          <div className={styles.menu}>
-            <ul>
-              {tabs.map(tab => (
-                <li key={tab.id}>
-                  <a href='#'>{tab.name}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.product}>
-            <img src={products[0].img} alt='' />
-            <FurnitureGalleryActions />
-            <FurnitureGalleryPrice
-              name={products[0].name}
-              price={products[0].price}
-              promoPrice={products[0].promoPrice}
-              stars={products[0].stars}
-            />
-          </div>
-          <div className={styles.slider}>
-            <div className={styles.arrow}>
-              <a href='#'>&#x3c;</a>
-            </div>
-            <div className={styles.thumbnails}>
-              {products.slice(0, 6).map(product => (
-                <div key={product.id} className={styles.thumbnail}>
-                  <img src={product.img} alt=''></img>
+class FurnitureGallery extends React.Component {
+  render() {
+    const { products, tabs } = this.props;
+
+    return (
+      <div className={styles.root}>
+        <div className='container'>
+          <div className='row'>
+            <div className={`col-12 col-md-12 col-lg-6 ${styles.gallery}`}>
+              <h3>Furniture Gallery</h3>
+              <div className={styles.menu}>
+                <ul>
+                  {tabs.map(tab => (
+                    <li key={tab.id}>
+                      <a href='#'>{tab.name}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.product}>
+                <img src={products[0].img} alt='' />
+                <FurnitureGalleryActions />
+                <FurnitureGalleryPrice
+                  name={products[0].name}
+                  price={products[0].price}
+                  promoPrice={products[0].promoPrice}
+                  stars={products[0].stars}
+                />
+              </div>
+              <div className={styles.slider}>
+                <div className={styles.arrow}>
+                  <a href='#'>&#x3c;</a>
                 </div>
-              ))}
+                <div className={styles.thumbnails}>
+                  {products.slice(0, 6).map(product => (
+                    <div key={product.id} className={styles.thumbnail}>
+                      <img src={product.img} alt=''></img>
+                    </div>
+                  ))}
+                </div>
+                <div className={styles.arrow}>
+                  <a href='#'>&#x3e;</a>
+                </div>
+              </div>
             </div>
-            <div className={styles.arrow}>
-              <a href='#'>&#x3e;</a>
-            </div>
+            <PromoProduct />
           </div>
         </div>
-        <PromoProduct />
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 FurnitureGallery.propTypes = {
   products: PropTypes.arrayOf(
